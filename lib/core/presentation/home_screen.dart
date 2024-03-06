13db49350841668d00a6f0f702fb9d77e1e6ff45
sacrilegioushero220 <sandweepsabu@gmail.com>
2024-03-06 11:18:58 +0530

    reworked HomeScreen for handling the error page

--- lib/core/presentation/home_screen.dart
+++ lib/core/presentation/home_screen.dart
@@ -16,98 +16,99 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
+    // Fetch popular videos when the screen is initialized
     context.read<ApiBloc>().add(PopularVideosFetchEvent());
   }
 
   @override
   Widget build(BuildContext context) {
-    return RefreshIndicator(
-      onRefresh: () async {
-        context.read<ApiBloc>().add(PopularVideosFetchEvent());
+    return BlocBuilder<ApiBloc, ApiState>(
+      builder: (context, state) {
+        return Scaffold(
+          body: _buildBody(state),
+        );
       },
-      child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.only(
-            left: 18,
-            right: 18,
-          ),
-          child: Column(
-            children: [
-              const CustomSearchBar(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Home",
-                    style: GoogleFonts.beVietnamPro(
-                      color: const Color(0xFFA6A6A6),
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
-                      height: 0,
-                    ),
-                  ),
-                  Text(
-                    "Popular Videos",
-                    style: GoogleFonts.beVietnamPro(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                      height: 0,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Expanded(
-                child: BlocBuilder<ApiBloc, ApiState>(
-                  builder: (context, state) {
-                    if (state is ApiLoadingState) {
-                      return const Center(child: CircularProgressIndicator());
-                    } else if (state is ApiSuccessState) {
-                      // Assuming you have a list of Video objects in state
-                      final videos = state.video;
-                      return ListView.builder(
-                        itemCount: videos.length,
-                        itemBuilder: (context, index) {
-                          final video = videos[index];
-
-                          return HomeTile(
-                            isVideo: false,
-                            tilePic: video.videoImage ?? "",
-                            categoryTitle: video.categoryName ?? '',
-                            title: video.videoTitle ?? '',
-                            subtitle: video.postedBy ?? '',
-                            uri: video.videos,
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (ctx) => VideoDetailScreen(
-                                    isSearchNeeded: false,
-                                    video: video,
-                                    title: 'Popular Videos',
-                                  ),
-                                ),
-                              );
-                            },
-                          );
-                        },
-                      );
-                    } else {
-                      // Handle error state
-                      return const Center(
-                        child: Text('Failed to fetch videos'),
-                      );
-                    }
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
+  }
+
+  Widget _buildBody(ApiState state) {
+    if (state is ApiLoadingState) {
+      // Loading state
+      return const Center(child: CircularProgressIndicator());
+    } else if (state is ApiSuccessState) {
+      // Success state
+      final videos = state.video;
+
+      return Padding(
+        padding: const EdgeInsets.only(
+          left: 18,
+          right: 18,
+        ),
+        child: Column(
+          children: [
+            const CustomSearchBar(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Home",
+                  style: GoogleFonts.beVietnamPro(
+                    color: const Color(0xFFA6A6A6),
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    height: 0,
+                  ),
+                ),
+                Text(
+                  "Popular Videos",
+                  style: GoogleFonts.beVietnamPro(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    height: 0,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: videos.length,
+                itemBuilder: (context, index) {
+                  final video = videos[index];
+
+                  return HomeTile(
+                    isVideo: false,
+                    tilePic: video.videoImage ?? "",
+                    categoryTitle: video.categoryName ?? '',
+                    title: video.videoTitle ?? '',
+                    subtitle: video.postedBy ?? '',
+                    uri: video.videos,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (ctx) => VideoDetailScreen(
+                            isSearchNeeded: false,
+                            video: video,
+                            title: 'Popular Videos',
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      );
+    } else {
+      // Error state
+      context.read<ApiBloc>().add(PopularVideosFetchEvent());
+      return const SizedBox();
+    }
   }
 }
