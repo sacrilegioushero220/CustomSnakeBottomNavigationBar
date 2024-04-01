57a9581ef91578a6c9dc9ae0bbfb54d4a3036eba
sacrilegioushero220 <sandweepsabu@gmail.com>
2024-04-01 21:59:14 +0530

    changed

--- lib/flutter_snake_navigationbar.dart
+++ lib/flutter_snake_navigationbar.dart
@@ -1,4 +1,4 @@
-library custom_snake_bottom_navigation_bar;
+library flutter_snake_navigationbar;
 
 export 'package:flutter_snake_navigationbar/src/snake_bar_widget.dart';
 export 'package:flutter_snake_navigationbar/src/theming/snake_shape.dart';
